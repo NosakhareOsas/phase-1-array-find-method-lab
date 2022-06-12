fdfc6d386fa26f45e6ae8fe94a3cf52e706aba91
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-12 12:26:35 +0100

    Test complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const winner = (item) => {
+    return (item.result === "W");
+}
+
+//const year = obj => obj.find(winner);
+
+const superbowlWin = (array) => {  
+    const item = array.find(winner);
+    if (item === undefined){
+        return undefined;
+    }else{
+        const value = Object.values(item);
+        return value[0];
+    }   
+};
